16864632ddd721dc57c71587560368f5854e184f
Temesgen dora <tdemeke36@gmail.com>
2024-06-06 03:39:43 -0700

    fixed: task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" type-annotated add function"""
+
+
+def add(a: float, b: float) -> float:
+    """
+
+    Args:
+        a (float): _description-
+        b (float): _description_
+
+    Returns:
+        float: sum of a and b
+    """
+    return float
