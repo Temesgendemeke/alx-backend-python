ed8ad42f9cfd0b28707183ebb33fb47579804dc4
Temesgen dora <tdemeke36@gmail.com>
2024-06-10 03:39:47 -0700

    task 0 fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -13,6 +13,6 @@ async def wait_random(max_delay: int = 10) -> float:
     Returns:
         _type_: _description_
     """
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(max_delay)
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
     return delay
