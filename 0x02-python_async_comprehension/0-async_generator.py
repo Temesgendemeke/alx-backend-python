6c44a3c946b6f7b9dd2a6e46bcd4d7e0ed5168ea
Temesgen dora <tdemeke36@gmail.com>
2024-06-11 08:53:16 -0700

    fixed task 0 #3

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """async_generator"""
 import random
 import asyncio
-from typing import Iterator
+from typing import AsyncIterator
 
 
-async def async_generator() -> Iterator[float]:
+async def async_generator() -> AsyncIterator[float]:
     """async_generator"""
     for _ in range(10):
         await asyncio.sleep(1)
