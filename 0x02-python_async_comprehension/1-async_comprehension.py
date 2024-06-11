0b8dbde9d350c65e6a241f1e8951b8657460f924
Temesgen dora <tdemeke36@gmail.com>
2024-06-11 09:06:20 -0700

    fixed task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,7 +3,6 @@
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> float:
     """async comprehension function"""
-    random_numbers = [i async for i in async_generator()]
-    return random_numbers
+    return [i async for i in async_generator()]
